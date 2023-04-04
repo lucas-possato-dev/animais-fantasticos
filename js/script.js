2433f5d1e7fa98e290f65cabf36c60669c2888c0
lucas-possato-dev <lucaspstinho@gmail.com>
2023-04-04 09:07:25 -0300

    accordion-class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import initAccordion from './modules/accordion-animação.js';
+import Accordion from './modules/accordion-animação.js';
 import initTabNav from './modules/tabNav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -11,9 +11,10 @@ import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimaScroll from './modules/scroll-animação.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+const accordion = new Accordion('[data-anime="accordion"] dt');
 
 scrollSuave.init();
-initAccordion();
+accordion.init();
 initTabNav();
 initModal();
 initTooltip();
