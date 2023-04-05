5cb6fff5f504feccdef232da42dd9ec943629ae5
lucas-possato-dev <116115210+lucas-possato-dev@users.noreply.github.com>
2023-04-05 18:09:14 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './js/script.js',
   output: {
-    path: path.resolve(__dirname, './public'),
+    path: path.resolve(__dirname, '../public'),
     filename: 'main.js',
   },
   module: {
