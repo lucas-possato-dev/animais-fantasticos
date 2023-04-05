9036412c511abb8a87bbb352c35f5e107d83fd6e
lucas-possato-dev <lucaspstinho@gmail.com>
2023-04-05 11:44:11 -0300

    correção caminho fetch-animais

--- js/script.js
+++ js/script.js
@@ -30,5 +30,5 @@ dropdownMenu.init();
 menuMobile.init();
 funcionamento.init();
 
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
